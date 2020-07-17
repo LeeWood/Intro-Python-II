99abc7d75069aa0e6899f3681f695fd1e73f3572
lee-wood <leemaewood@gmail.com>
2020-07-16 22:28:50 -0400

    changed script for room class to fit addition of items attr

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,26 @@
 from room import Room
 from player import Player
+from items import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -61,11 +62,11 @@ player1 = Player('Link', room['outside'])
 def adventureGameStart():
     #welcome message
     print(f'Welcome, {player1.name}!')
-    print(f'You find yourself at a mysterious cave entrance...how will you begin your adventure?')
+    print(f'This is the beginning of your greatest feat yet! {player1.current_room.description}...how will you begin your adventure?')
     #* here if user goes in any direction other than north, print error message..."Are you sure?"
 
     while True:
-        move = input('Which direction do you want to go? \n[N] North  [S] South  [E] East  [W] West  [Q] Quit \n :').upper()
+        move = input('What would you like to do? \n[N] Go North  [S] Go South  [E] Go East  [W] Go West  [Q] Quit \n :').upper()
         #* need to chage this so that user input is NOT case sensitive
 
         if move == "N" and hasattr(player1.current_room, 'n_to'):
@@ -89,7 +90,7 @@ def adventureGameStart():
             break
 
         else:
-            print(f'\nThere is nothing here. Try going a different direction.')
+            print(f"\nYou can't go this way. Try going a different direction.")
 
         #* I need error handling for if there is no option to go a certain direction based on the player's current position.    
 
--- src/room.py
+++ src/room.py
@@ -1,9 +1,13 @@
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
     def __str__(self):
-        return f'You are now in the {self.name}. {self.description}'
+        if len(self.items) < 0:
+            return f'You are now in the {self.name}. {self.description}. You look around and see {self.items}'
+        else:
+            return f'You are now in the {self.name}. {self.description} You notice nothing else in this area.'
 
 
         
